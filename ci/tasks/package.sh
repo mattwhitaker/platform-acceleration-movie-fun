1c5a12a4a3150d9b9153a4f1526faf07a55e2343
matthewwhitaker <matthew.whitaker@mimacom.com>
2017-01-16 15:07:21 -0700

    update package

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -3,11 +3,11 @@
 set -ex
 
 pushd movie-fun-source
-  echo "Packaging JAR"
+  echo "Packaging WAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find movie-fun-source/target -type f -name *.jar | wc -l`
+jar_count=`find movie-fun-source/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
